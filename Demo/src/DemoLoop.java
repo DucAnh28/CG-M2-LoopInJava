7b3851814e88869250bcdd0b6479aa8b90a7ed0e
Duc Anh Nguyen <ducanhnguyeny2k@gmail.com>
2022-07-03 21:45:46 +0700

    Add Demo File

---
+++ Demo/src/DemoLoop.java
@@ -0,0 +1,7 @@
+public class DemoLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 8; i++) {
+            System.out.println("Chi Kieu Anh Xinh !");
+        }
+    }
+}
